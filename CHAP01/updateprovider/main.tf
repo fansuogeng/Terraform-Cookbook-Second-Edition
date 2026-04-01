581dec400a6aaf7159f98663de2dc3380e5f3320
Michael Geng <fansgeng@gmail.com>
2026-04-01 20:14:46 +1100

    ch1 update provider

--- CHAP01/updateprovider/main.tf
+++ CHAP01/updateprovider/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.67.0"
+      version = "~> 3.0.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -17,5 +17,5 @@ resource "random_string" "password" {
   special = false
   upper   = false
   lower   = true
-  number  = false
+  numeric  = false
 }
